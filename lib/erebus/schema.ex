80255f3884c1f270e63daa5565aeefad9867df9e
Piotr Szmielew <p.szmielew@ava.waw.pl>
2021-05-11 18:30:37 +0200

    fix type in ecto schema

--- lib/erebus/schema.ex
+++ lib/erebus/schema.ex
@@ -1,22 +1,22 @@
 defmodule Erebus.Schema do
   defmacro hashed_encrypted_field(name) do
     quote do
-      field unquote(name), :string, virtual: true
-      field String.to_atom("#{unquote(name)}_encrypted"), :map
-      field String.to_atom("#{unquote(name)}_hash"), :string
+      field(unquote(name), :string, virtual: true)
+      field(String.to_atom("#{unquote(name)}_encrypted"), :binary)
+      field(String.to_atom("#{unquote(name)}_hash"), :string)
     end
   end
 
   defmacro encrypted_field(name) do
     quote do
-      field unquote(name), :string, virtual: true
-      field String.to_atom("#{unquote(name)}_encrypted"), :map
+      field(unquote(name), :string, virtual: true)
+      field(String.to_atom("#{unquote(name)}_encrypted"), :binary)
     end
   end
 
   defmacro data_encryption_key() do
     quote do
-      field :dek, :map
+      field(:dek, :map)
     end
   end
 end
